1f94cc52194567121d86e34dc14079e5a1517444
TheENDima <theendima@gmail.com>
2026-04-12 11:21:55 +0300

    циклы и операторы

---
+++ циклы и операторы 2.py
@@ -0,0 +1,10 @@
+#i = 5
+
+#while i  <= 15:  #условие
+#    print(i)
+#    i += 2 #(шаг добавление)
+ishascar = True
+
+while ishascar:
+    if input("Enter data") == "Stop":
+        ishascar = False
---
+++ циклы и операторы 4.py
@@ -0,0 +1,9 @@
+fount = None
+for i in "hello":
+    if i == "l":
+        fount = True
+        break             # как только находим нужную буквоку/цикорку выходим из цикла
+    else:                 # tckb yf[jl;bn
+        fount = False
+
+print(fount)
