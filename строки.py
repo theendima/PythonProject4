72d6c14cf198d73a12fdf0eaa313f9e8b6f97f17
TheENDima <theendima@gmail.com>
2026-04-15 23:41:31 +0300

    options

--- строки.py
+++ строки.py
@@ -1,2 +1,17 @@
-word = "itproger"
-print(len(word))
+# word = "Boxing, Well, dOne,"
+# #print(word.count("p")) #ищет количество букв
+# #print(word.capitalize()) #показывает количество
+# #print(word.find("pr")) #показывает 1 позицию где нашел букву (индек файл ищет нужную строку).
+# #print(word.split("# "))
+# hubby = word.split(", ")
+# # print(hubby[1])
+# for i in range(len(hubby)): #цикл for  в элементе element  в списке hubby
+#     hubby[i] = hubby[i].capitalize()
+# result = ", ".join(hubby) # join метод для переменоой листа word
+# print(result)
+
+# word = "football"
+# print(word [0:2])
+lis = [6, 2, 4, True, 1.54] # переменная lis  с  данной строчки
+print(lis[2:5:2]) #индексы и срезы
+print(lis[::-1]) # перевернутая строка
