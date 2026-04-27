055cd658be31ec1f749275a567935c7b44e36a2a
TheENDima <theendima@gmail.com>
2026-04-27 23:08:55 +0300

    with as and more

---
+++ modules.py
@@ -0,0 +1,25 @@
+# import time # встроенный модуль
+#
+# time.sleep(5) # вызываем модул ьс джоп ко нмадной + указывааем задуржку сколько бюудет ждать (5секунд)
+# print("спим 5 сек")
+# import datetime as dt # вызвали модуль дата тайм и переименовали его в dt
+#
+# day = dt.datetime.today().time()  #вызвал переменную и запросил  сегодня/ сейчас уточнил что нужно  время
+# print(day)
+
+# import datetime as d, sys, os, platform, random
+# print(sys.path)
+# print(os.name)
+# print(platform.system())
+# print(random())
+
+# from math import sqrt as dd, ceil # из модуля импортируем  часть функций используем фром и что импортируем
+# print(dd(100))
+
+# import mymodule as my
+# print(my.name)
+# my.hello()
+
+from mymodule import add_three_numbers as add
+
+print(add(5,6,1))
---
+++ mymodule.py
@@ -0,0 +1,9 @@
+name = "Dima"
+def hello():
+    print("Hello", name)
+
+def add_three_numbers(a, b, c):
+    if a !=0 and b != 0 and c != 0:
+        return a + b + c
+    else:
+       return "some zero"
